755ffc45a61f20d0f33bbca910b85202b43c3812
Djebreds <refiahmad.djebreds@gmail.com>
2024-12-09 01:37:18 +0700

    faet: validate previous record must be complete

--- app/services/sleep_record_service/create_clock_in.rb
+++ app/services/sleep_record_service/create_clock_in.rb
@@ -8,6 +8,8 @@ module SleepRecordService
     end
 
     def call
+      validate_previous_record(@user)
+
       create_sleep_record!(@user)
     end
 
@@ -17,6 +19,14 @@ module SleepRecordService
 
     def create_sleep_record!(user)
       user.sleep_records.create!(clock_in: Time.current)
+    end
+
+    def validate_previous_record(user)
+      previous_record = user.sleep_records.where(clock_out: nil).last
+
+      return unless previous_record
+
+      raise Exceptions::GeneralError.new(I18n.t('sleep_record.record_not_completed'), :bad_request, 400)
     end
   end
 end

1d15e4cb658f16aa570bd3d63a4393f87250bb8d
Djebreds <refiahmad.djebreds@gmail.com>
2024-12-08 18:05:25 +0700

    feat: create session model

---
+++ app/models/session.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# Represents a session in the application
+class Session < ApplicationRecord
+  belongs_to :user
+
+  before_create :generate_token
+
+  def regenerate_token!
+    if frozen?
+      dup.regenerate_token!
+    else
+      generate_token
+      save!
+    end
+  end
+
+  private
+
+  def generate_token
+    self.token ||= SecureRandom.hex(32)
+  end
+end
